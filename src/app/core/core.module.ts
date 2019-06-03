6421a247db7e460249c01818ab57934e45ef453c
Chiara Martignano <chiaramartignano@gmail.com>
2019-06-03 16:38:41 +0200

    Remove unused component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileUploadComponent } from './file-upload/file-upload.component';
-import { ResultComponent } from './result/result.component';
 import { CollationComponent } from './collation/collation.component';
 import { SharedModule } from '../shared/shared.module';
 import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  declarations: [FileUploadComponent, ResultComponent, CollationComponent],
-  exports: [FileUploadComponent, ResultComponent, CollationComponent],
+  declarations: [FileUploadComponent, CollationComponent],
+  exports: [FileUploadComponent, CollationComponent],
   imports: [
     CommonModule,
     SharedModule,
